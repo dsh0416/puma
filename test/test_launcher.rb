4c5e695d33fbf7992a6dff4d9f44f5fd16f61d35
Will Jordan <will@code.org>
2020-04-11 21:56:20 -0700

    Add tests for Puma.stats

--- test/test_launcher.rb
+++ test/test_launcher.rb
@@ -1,6 +1,7 @@
 require_relative "helper"
 
 require "puma/configuration"
+require 'puma/events'
 
 class TestLauncher < Minitest::Test
   def test_dependencies_and_files_to_require_after_prune_is_correctly_built_for_no_extra_deps
@@ -76,6 +77,34 @@ class TestLauncher < Minitest::Test
     assert_equal File.read(tmp_path).strip.to_i, Process.pid
 
     File.unlink tmp_path
+  end
+
+  def test_puma_stats
+    conf = Puma::Configuration.new {|c| c.clear_binds!}
+    launcher = launcher(conf)
+    launcher.events.on_booted {launcher.stop}
+    launcher.run
+    Puma::Server::STAT_METHODS.each do |stat|
+      assert_includes Puma.stats, stat
+    end
+  end
+
+  def test_puma_stats_clustered
+    status = nil
+    conf = Puma::Configuration.new do |c|
+      c.workers 1
+      c.clear_binds!
+    end
+    launcher = launcher(conf)
+    Thread.new do
+      sleep Puma::Const::WORKER_CHECK_INTERVAL + 1
+      status = Puma.stats[:worker_status].first[:last_status]
+      launcher.stop
+    end
+    launcher.run
+    Puma::Server::STAT_METHODS.each do |stat|
+      assert_includes status, stat
+    end
   end
 
   private
